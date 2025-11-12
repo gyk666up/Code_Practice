105ecfa07e878e6923c1b8a3c1cb070a420d50ea
gyk666 <2042837972@qq.com>
2025-11-12 17:52:37 +0800

    重启

---
+++ 洛谷/基础数学问题/P_1029_NOIP_2001_普及组_最大公约数和最小公倍数问题.cpp
@@ -0,0 +1,55 @@
+// //没思路🤔😑
+
+// //方法一 使用自带的函数__gcd()最大公因数
+// //该题的关键点在于，两个数的积等于它们最大公约数和它们最小公倍数的积。
+// //公式表示为 a×b=gcd(a,b)×lcm(a,b)
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define int long long
+// int ans=0;
+// signed main()
+// {
+//     ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//     int x,y;cin>>x>>y;
+// 这行代码不太容易理解 最下面的那行代码好理解
+//     if(x==y)ans--;
+//     y*=x;
+//     for(int i=1;i<=sqrt(y);i++)
+//     {
+//         if(y%i==0&&__gcd(i,y/i)==x)ans+=2;//交换顺序也可以 因此是两种
+//     }
+//     cout<<ans;
+//     return 0;
+// }
+
+
+// // //方法2 与方法一一样只是手写gcd函数
+// // //有一个这样的定律gcd(a,b)=gcd(b,a%b)
+// // inline int gcd(int x,int y)
+// // {
+// //     if(y==0)return x;
+// //     return gcd(y,x%y);
+// // }
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    int x,y;cin>>x>>y;
+    y*=x;
+    int ans=0;
+    for(int i=1;i<=sqrt(y);i++)
+    {
+        if(y%i==0&&__gcd(y/i,i)==x)
+        {
+            if(y/i!=i)ans+=2;
+            else ans+=1;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
